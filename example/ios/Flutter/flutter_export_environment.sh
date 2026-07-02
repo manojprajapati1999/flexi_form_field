9da315d30aa6da4620fd70d993330bd79e9e9a62
manojprajapati1999 <patadiyamanoj4@gmail.com>
2026-07-02 11:38:00 +0530

    4.0.3
    fix pub point

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=I:\FlutterSDK3-32-6-Dart-3-8-1"
+export "FLUTTER_ROOT=C:\Users\ManojTechFirst\fvm\default"
 export "FLUTTER_APPLICATION_PATH=F:\manoj\flutter\package\flexi_form_field\example"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=F:\manoj\flutter\package\flexi_form_field\example\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
